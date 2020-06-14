e31673a95132e7a908b523c204d38f33786e08b4
Ramya Gangula <ramya1331@gmail.com>
2020-06-14 17:46:58 -0500

    Update run_waitress_server.py

--- run_waitress_server.py
+++ run_waitress_server.py
@@ -1,6 +1,6 @@
 import os
 from waitress import serve
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangosite.djangosite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangosite.settings")
 application = get_wsgi_application()
 serve(application,host="0.0.0.0",port=os.environ["PORT"])
